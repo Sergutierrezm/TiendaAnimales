d29b43fa9cf2008db3459166cbf3b742cb451def
Sergutierrezm <sergiogutierrez370@gmail.com>
2025-06-02 23:45:23 +0200

    Update tienda.py

--- tienda.py
+++ tienda.py
@@ -1,32 +1,28 @@
 from typing import no_type_check
 from datetime import datetime
 
-
-
-
-
+#Inventario realizado mendiante un Diccionario
 inventario = {
     "perro": 10,
     "gato": 8,
     "pajaros": 10
 }
 cantidad_animal = 0
-
 for val in inventario.values():
     cantidad_animal += val
 
 compras = []
 
-
+#Saludos al usuario despues de registrar el nombre y apellidos
 print("Bienvenido a la tienda de mascotas")
-
 print("Indica tu nombre")
 nombre = input()
 print("indica tu apellido")
 apellido = input()
-
 nombre_completo = nombre + " " + apellido
 print("Encatando", nombre_completo)
+
+
 #Menu con opciones
 def MenuDelPrograma():
     print("Selecciona la opcion que deseas")
@@ -41,7 +37,7 @@ def mostrar_inventario():
     for llave, valor in inventario.items():
         print(f"{llave}: {valor}")
     
-
+#Funcion para que el usuario pueda comprar un animal
 def comprar_animal ():
     carrito = []
     while True:
@@ -69,12 +65,15 @@ def comprar_animal ():
     fecha = datetime.now()
     compras.append((nombre, carrito,fecha))
 
+#Funcion para revisar el historial de compras de animales con fecha y nombre del usuario
 def mostrar_compras():
     print("")
     print("Compras realizadas")
     for compra in compras:
         print(f"   {compra[0]} compró {compra[1]} en  {compra[2]}")
 
+
+#Logica del programa asociada con las funciones
 while True:
     MenuDelPrograma()
     respuesta = int(input())
